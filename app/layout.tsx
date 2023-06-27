a3596ef632f71aeca353a43eba660dd25c498b3a
YongYu <73355611+YneYme@users.noreply.github.com>
2023-06-27 22:15:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "yongyu_ChatGPT+Midjourney",
+  title: "Yongyu-ChatGPT+Midjourney",
   description: "Yong Yu's chatbot",
   viewport: {
     width: "device-width",
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "yongyu_ChatGPT+Midjourney",
+    title: "Yongyu-ChatGPT+Midjourney",
     statusBarStyle: "default",
   },
 };
